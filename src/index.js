a34ebb3403c99796127d02dbf8412a7f1286d210
KalashnikovKV <kirillrik1994@gmail.com>
2022-08-12 17:10:58 +0300

    feat: add morse-decoder task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,45 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+decode(expr)
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split('**********').map((el) => el.match(/.{1,10}/g))
+    let arrLast = []
+    let q = [];
+    let arrLastL = []
+    for (let i = 0; i < arr.length; i++) {
+         q.push(arr[i].map((el) => el.match(/.{1,2}/g)))
+    }
+    for (let i = 0; i < q.length; i++) {
+        arrLast.push(q[i].map((el) => el.map((el1) => {
+            if(el1 === '00'){
+                return 1
+            }
+
+            if (el1 === '10') {
+                return el1 = '.'
+            }
+            if(el1 === '11'){
+                return el1 = '-'
+            }
+        }).filter(function(f) { return f !== 1 }).reduce((a,b) => a.concat(b))
+        )
+        )
+    }
+
+    for (let i = 0; i < arrLast.length; i++) {
+        arrLastL.push(arrLast[i].map((el) => {
+
+            for (const key in MORSE_TABLE) {
+                if(el){
+                    return MORSE_TABLE[el]
+                }
+            }
+        }).reduce((a,b) => a.concat(b))
+        )
+    }
+    return arrLastL.join(' ')
 }
 
 module.exports = {
